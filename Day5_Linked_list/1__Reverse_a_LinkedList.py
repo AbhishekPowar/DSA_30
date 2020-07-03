62075d43a5f702ecf0319f8100ac0916e6e10f07
abhishek <mailabhipoear@gmail.com>
2020-07-03 19:06:02 +0530

    reverserecursive and iteratibe

---
+++ Day5_Linked_list/1__Reverse_a_LinkedList.py
@@ -0,0 +1,38 @@
+# 1__Reverse_a_LinkedList
+from dataStructure.linkedList import singlyLinkedList, Node
+
+
+class sLL(singlyLinkedList):
+    def __init(self):
+        super().__init__(self)
+
+    def recur(self):
+        cur = self.head
+        if self.head == None or self.head.next == None:
+            return self.head
+        else:
+            def reverse(node):
+                if node.next == None:
+                    return node
+                nxt = node.next
+                rem, nxt.next = reverse(nxt), node
+                return rem
+            cur = self.head
+            self.head = reverse(self.head)
+            cur.next = None
+
+    def rev(self):
+        prev = None
+        cur = self.head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        self.head = prev
+
+
+ll = sLL()
+for i in range(1, 6):
+    ll.add(i)
+print(ll)
+# ll.rev()
+ll.recur()
+print(ll)
