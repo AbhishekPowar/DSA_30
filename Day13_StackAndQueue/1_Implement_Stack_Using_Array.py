e2cda53f6c59afae6f311f39ab523fe330e53e12
AbhishekPowar <mailabhipowar@gmail.com>
2021-09-29 23:56:35 +0530

    stack and queue implementation using array

---
+++ Day13_StackAndQueue/1_Implement_Stack_Using_Array.py
@@ -0,0 +1,75 @@
+# Implement_Stack
+
+class Stack:
+    def __init__(self, size=8, isExpandable=False):
+        self.isExpandable = isExpandable
+        self.maxSize = size
+        self.dataArr = [0] * self.maxSize
+        self.top = -1
+        self.len = 0
+
+    def getSize(self):
+        '''Internal array size'''
+        return self.maxSize
+
+    def length(self):
+        '''Stack size'''
+        return self.len
+
+    def push(self, data):
+        if self.top < self.maxSize-1:
+            self.dataArr[self.top+1] = data
+            self.top += 1
+            self.len += 1
+        else:
+            if self.isExpandable:
+                temp = self.dataArr
+                self.dataArr = [0] * (self.maxSize * 2)
+                self.maxSize *= 2
+                for idx, x in enumerate(temp):
+                    self.dataArr[idx] = x
+                self.push(data)
+            else:
+                print(f"Stack full, cannot insert {data}")
+            # raise Exception("Stack full")
+
+    def pop(self):
+        if self.top > -1:
+            self.top -= 1
+            self.len -= 1
+        else:
+            print("Stack empty")
+            # raise Exception("Stack empty. Cannot pop")
+
+    def isEmpty(self):
+        return self.top == -1
+
+    def peek(self):
+        if self.top > -1:
+            return self.dataArr[self.top]
+        else:
+            print("Stack empty")
+            # raise Exception("Stack empty. Cannot pop")
+
+    def print(self):
+        print([self.dataArr[x] for x in range(0, self.top+1)])
+
+
+if __name__ == "__main__":
+    s = Stack(2, True)
+
+    s.print()
+
+    s.pop()
+
+    s.push(11)
+    s.push(12)
+    s.push(12)
+    s.push(12)
+    s.push(13)
+    print('Top =', s.peek())
+    print('Size =', s.getSize())
+    print('len =', s.length())
+
+    s.print()
+
---
+++ Day13_StackAndQueue/2_Implement_Queue_Using_Array.py
@@ -0,0 +1,80 @@
+# Implement_Queue
+
+
+class Queue:
+    def __init__(self, size=8, isExpandable=False):
+        self.isExpandable = isExpandable
+        self.maxSize = size
+        self.dataArr = [0] * self.maxSize
+        self.start = -1
+        self.end = -1
+        self.len = 0
+
+    def getSize(self):
+        return self.maxSize
+
+    def __len__(self):
+        return self.len
+
+    def push(self, data):
+        if self.len < self.maxSize:
+            if self.end == self.maxSize-1:
+                self.__rotateDataArray()
+            self.dataArr[self.end+1] = data
+            self.end += 1
+            self.len += 1
+        else:
+            if self.isExpandable:
+                if self.start != -1:
+                    self.__rotateDataArray()
+                temp = self.dataArr
+                self.dataArr = [0] * (self.maxSize * 2)
+                self.maxSize *= 2
+                for idx, x in enumerate(temp):
+                    self.dataArr[idx] = x
+                self.push(data)
+            else:
+                print(f"Queue full, cannot insert {data}")
+            # raise Exception("Queue full")
+
+    def __rotateDataArray(self):
+        for idx in range(0, self.len):
+            self.dataArr[idx] = self.dataArr[self.start+idx+1]
+        self.start = -1
+        self.end = self.len - 1
+
+    def pop(self):
+        if self.len > 0:
+            self.start += 1
+            self.len -= 1
+            return self.dataArr[self.start-1]
+        else:
+            print("Queue empty")
+            # raise Exception("Queue empty. Cannot pop")
+
+    def isEmpty(self):
+        return self.len == 0
+
+    def peek(self):
+        if self.len > 0:
+            return self.dataArr[self.start]
+        else:
+            print("Queue empty")
+            # raise Exception("Queue empty. Cannot pop")
+
+    def print(self):
+        print([self.dataArr[x] for x in range(self.start+1, self.end+1)])
+
+
+if __name__ == "__main__":
+    qu = Queue(size=2, isExpandable=False)
+    print(f'isEmpty = {qu.isEmpty()}')
+    qu.pop()
+    print(f'getSize = {qu.getSize()}')
+    [qu.push(x) for x in range(1, 5)]
+    print(f'len = {len(qu)}')
+    qu.pop()
+    qu.push(8)
+    print(f'peek = {qu.peek()}')
+    qu.print()
+
